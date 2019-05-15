7a60ab421a7a13bce4a915f1faa9c6bca26aa19b
Cosmo Kent <cosmolightfoot@gmail.com>
2019-05-15 10:58:27 -0700

    randomizer works, needs tests

---
+++ src/components/ColorDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ColorDisplay.css';
+
+export default function ColorDisplay({ color }) {
+  return (
+    <div className={styles.ColorDisplay} style={{ backgroundColor: color }}></div>
+  );  
+}
+
+ColorDisplay.propTypes = {
+  color: PropTypes.string.isRequired
+};
